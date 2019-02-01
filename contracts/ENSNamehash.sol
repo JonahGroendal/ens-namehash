60a6afe75725d807ca1a4a04f0a246bb24144556
JonahGroendal <jone951@gmail.com>
2019-02-01 01:59:21 -0500

    deploy to rinkeby

--- contracts/ENSNamehash.sol
+++ contracts/ENSNamehash.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.1;
+pragma solidity ^0.5.2;
 
 /*
  * @dev Solidity implementation of the ENS namehash algorithm.
--- truffle.js
+++ truffle.js
@@ -21,5 +21,21 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
     },
+    rinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "4",
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.2",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
   }
 };
