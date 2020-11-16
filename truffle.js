6154f343fa0628f46633e9db7c8b9cce89f90892
Jonah Groendal <13501607+JonahGroendal@users.noreply.github.com>
2020-11-16 16:34:11 -0500

    Remove more unneeded deployment code

--- truffle.js
+++ truffle.js
@@ -1,13 +1,3 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const fs = require('fs');
-
-const infuraKey = "48899b10645a48e189e345be4be19ece";
-
-let privateKeys;
-try {
- privateKeys = JSON.parse(fs.readFileSync("keys.json").toString().trim()).private;
-} catch {}
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
